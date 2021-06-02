2bfc0a4b08d6290d4795870cccdac68f664dcaaf
Lucian Louw <lucian_louw@hotmail.com>
2021-06-02 19:57:34 +0200

    New Content

---
+++ modal/script.js
@@ -0,0 +1,31 @@
+class Modal {
+    constructor(){
+        this.modal = document.querySelector(".modal")
+    }
+    open() {
+        this.modal.style.display = "initial"
+    }
+    close() {
+        this.modal.style.display = "none"
+    }
+    accept(){
+        this.modal.style.display = "none"
+    }
+}
+
+const openBtn = document.querySelector(".js-open");
+const closeBtn = document.querySelector(".js-close");
+const acceptBtn = document.querySelector(".js-accept")
+const modal = new Modal()
+
+
+openBtn.addEventListener('click',function(event){
+  modal.open()
+})
+
+closeBtn.addEventListener("click", function(event){
+  modal.close()
+})
+acceptBtn.addEventListener("click", function(event){
+  modal.accept()
+})
